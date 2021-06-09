a2b14945f6abfeee0097d65c8d26618547c6df3c
John Mitri <john.mitri.98@gmail.com>
2021-06-09 11:26:05 +0300

    test2

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          yo React
+          i like turtles React
         </a>
       </header>
     </div>
