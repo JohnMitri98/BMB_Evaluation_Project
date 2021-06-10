24db50dfbd04fc1dc57517e6cebcbef367a9b9e4
John Mitri <john.mitri.98@gmail.com>
2021-06-10 11:18:50 +0300

    login

--- App.js
+++ App.js
@@ -5,22 +5,13 @@ import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
   render() {
     return (
       <div style = {{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <h1>Hello</h1>
-        <Form onSubmit = {this.handleSubmit} />
+        <Form />
       </div>
     );
-  }
-
-  handleSubmit() {
-
   }
 
 }
--- Form.js
+++ Form.js
@@ -6,31 +6,46 @@ export default class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: "",
-            password: ""
+            Username: "",
+            Password: ""
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div style = {{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
                     <div>
                         <label>
                             Username
-                            <input type = "text" name = "Username" />
+                            <input type = "text" value = {this.state.Username} onChange = {this.handleUsernameChange} />
                         </label>
                     </div>
                     <div>
                         <label>
                             Password
-                            <input type = "text" name = "Password" />
+                            <input type = "password" value = {this.state.Password} onChange = {this.handlePasswordChange}/>
                         </label>
                     </div>
                     <input type = "submit" value = "Submit" onSubmit = {this.onSubmit}/>
                 </div>
             </form>
         )
+    }
+
+    handleSubmit(event) {
+        alert('Username: ' + this.state.Username + '\nPassword: ' + this.state.Password);
+    }
+
+    handleUsernameChange(event) {
+        this.setState({Username: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({Password: event.target.value});
     }
 
 }
