9f453156a5e59715eeaa798ce4009febd2766bbe
John Mitri <john.mitri.98@gmail.com>
2021-06-10 11:00:23 +0300

    login

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import React from 'react';
+import Form from './Form';
+import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <div style = {{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-        <h1>Hello World!</h1>
-        <h2>This is text.</h2>
+        <h1>Hello</h1>
+        <Form />
       </div>
     );
   }
 
 }
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ Form.js
@@ -0,0 +1,38 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+export default class Form extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+    }
+
+    render() {
+        return (
+            <form>
+                <div style = {{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+                    <div>
+                        <label>
+                            Username
+                            <input type = "text" name = "Username" />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Password
+                            <input type = "text" name = "Password" />
+                        </label>
+                    </div>
+                    <input type = "submit" value = "Submit" />
+                </div>
+            </form>
+        )
+    }
+
+}
+
+//ReactDOM.render(<Form />, document.getElementById('root'))
